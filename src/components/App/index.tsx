c9705b6c13b19331562028c8ca4516d3abb83637
Alex Aunter <lxaunter@gmail.com>
2022-02-13 18:38:07 +0300

    feat(App): add data propagation example

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from '../../pages/About';
 import Users from '../../pages/Users';
@@ -5,11 +6,20 @@ import UserLockups from '../../pages/UserLockups';
 import Header from '../Header';
 
 export default function App() {
+  const [contractState, setContractState] = useState({});
+
+  const loadAllData = () => {
+    console.log('UPDATING');
+    setContractState({ name: 'name' });
+  };
+
+  setTimeout(loadAllData, 5000);
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<About />} />
+        <Route path="/" element={<About contractState={contractState} />} />
         <Route path="/users" element={<Users />} />
         <Route path="/users/:userId" element={<UserLockups />} />
       </Routes>
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,15 +1,21 @@
 import { config } from '../../config';
 
-export default function About() {
+export default function About({ contractState }: { contractState: { name?: string } }) {
   return (
     <div>
       <h1>About Page</h1>
 
-      This page should render all useful info about contract,
-      at least ft-lockup name, token name, contract ft balance,
+      <pre>
+        {`
+          props value from parent controller: count=${contractState.name || 'LOADING'}
 
-      ft-lockup account id:
-      {config.contractName}
+          This page should render all useful info about contract,
+          at least ft-lockup name, token name, contract ft balance,
+
+          ft-lockup account id:
+          ${config.contractName}
+        `}
+      </pre>
     </div>
   );
 }
