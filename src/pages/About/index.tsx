4b47cdcadc2a65faaf17198f91e318512aef7f93
Alex Aunter <lxaunter@gmail.com>
2022-02-13 23:13:27 +0300

    feat(about): cleanup about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,22 +1,18 @@
 import { config } from '../../config';
 
-export default function About(
-  { contractState }: { contractState: { name?: string, lockups?: any[] } },
-) {
+export default function About() {
   return (
     <div>
       <h1>About Page</h1>
 
       <pre>
         {`
-          props value from parent controller: count=${contractState.name || 'LOADING'}
-
           This page should render all useful info about contract,
-          at least ft-lockup name, token name, contract ft balance,
+          at least ft-lockup name, token name, contract ft balance.
 
           ft-lockup account id: ${config.contractName}
 
-          Raw contract data: ${JSON.stringify(contractState.lockups, null, 2)}
+          TODO: expose token name in ft-lockup contract
         `}
       </pre>
 
