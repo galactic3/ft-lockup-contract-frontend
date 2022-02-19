bd79422bf3a71fb3be14f919624d3cac5c14a763
Alex Aunter <lxaunter@gmail.com>
2022-02-19 18:31:32 +0300

    test(smoke): disable netowrk, add token name test

--- src/NearApp.test.tsx
+++ src/NearApp.test.tsx
@@ -3,6 +3,24 @@ import { render, screen } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import NearApp from './NearApp';
 
+jest.mock('./services/near', () => ({
+  ...jest.requireActual('./services/near'),
+  connectNear: async () => {
+    return {
+      config: {},
+      api: {
+        getTokenAccountId: () => 'token.jest.testnet',
+        loadAllLockups: async () => {
+          return Promise.resolve([]);
+        }
+      },
+      signedIn: false,
+      signedAccountId: '',
+    }
+  }
+}));
+
+
 it('renders welcome message', async () => {
   await act(async () => {
     render(<NearApp />);
@@ -10,4 +28,7 @@ it('renders welcome message', async () => {
   });
   const items = await screen.findAllByText(/About Page/);
   expect(items).toHaveLength(1);
+
+  const tokenMessage = await screen.findAllByText(/token account id: /);
+  expect(screen.getByText(/token.jest.testnet/)).toBeInTheDocument();
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+// import * as JestFetchMock from 'jest-fetch-mock';
+
+beforeEach(() => {
+  global.fetch = jest.fn(() => { Prmose.reject("NETWORK ACCESS IS NOT ALLOWED IN TESTS") });
+});
