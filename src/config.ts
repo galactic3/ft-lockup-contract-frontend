4c1273e66bb21b13daa2ee7e10191337a2c82a67
backail <near.dkachur@gmail.com>
2022-04-08 20:22:41 +0400

    Add token contract env variable

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,14 @@ export interface INearConfig {
   networkId: string,
   nodeUrl: string,
   contractName: string,
+  tokenContractName: string,
   walletUrl: string,
   helperUrl: string,
   explorerUrl: string,
 }
 
 const CONTRACT_NAME = process.env.REACT_APP_CONTRACT_NAME || 'ft-lockup.demo000.ft-lockup.testnet';
+const TOKEN_CONTRACT_NAME = process.env.TOKEN_CONTRACT_NAME || 'token.demo000.ft-lockup.testnet'
 
 // TODO: move these data to envs
 function getConfig(): INearConfig {
@@ -19,6 +21,7 @@ function getConfig(): INearConfig {
         networkId: 'mainnet',
         nodeUrl: 'https://rpc.mainnet.near.org',
         contractName: CONTRACT_NAME,
+        tokenContractName: TOKEN_CONTRACT_NAME,
         walletUrl: 'https://wallet.near.org',
         helperUrl: 'https://helper.mainnet.near.org',
         explorerUrl: 'https://explorer.mainnet.near.org',
@@ -29,6 +32,7 @@ function getConfig(): INearConfig {
         networkId: 'testnet',
         nodeUrl: 'https://rpc.testnet.near.org',
         contractName: CONTRACT_NAME,
+        tokenContractName: TOKEN_CONTRACT_NAME,
         walletUrl: 'https://wallet.testnet.near.org',
         helperUrl: 'https://helper.testnet.near.org',
         explorerUrl: 'https://explorer.testnet.near.org',
@@ -38,6 +42,7 @@ function getConfig(): INearConfig {
         networkId: 'betanet',
         nodeUrl: 'https://rpc.betanet.near.org',
         contractName: CONTRACT_NAME,
+        tokenContractName: TOKEN_CONTRACT_NAME,
         walletUrl: 'https://wallet.betanet.near.org',
         helperUrl: 'https://helper.betanet.near.org',
         explorerUrl: 'https://explorer.betanet.near.org',
@@ -50,6 +55,7 @@ function getConfig(): INearConfig {
         nodeUrl: 'http://localhost:3030',
         walletUrl: 'http://localhost:4000/wallet',
         contractName: CONTRACT_NAME,
+        tokenContractName: TOKEN_CONTRACT_NAME,
       };
     case 'test':
     case 'ci':
@@ -60,6 +66,7 @@ function getConfig(): INearConfig {
         networkId: 'shared-test',
         nodeUrl: 'https://rpc.ci-testnet.near.org',
         contractName: CONTRACT_NAME,
+        tokenContractName: TOKEN_CONTRACT_NAME,
       };
     case 'ci-betanet':
       return {
@@ -69,6 +76,7 @@ function getConfig(): INearConfig {
         networkId: 'shared-test-staging',
         nodeUrl: 'https://rpc.ci-betanet.near.org',
         contractName: CONTRACT_NAME,
+        tokenContractName: TOKEN_CONTRACT_NAME,
       };
     default:
       throw Error(`Unconfigured environment '${env}'. Can be configured in src/config.js.`);
