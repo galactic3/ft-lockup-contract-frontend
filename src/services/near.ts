d103eeee869726c0237564c9f4eeb04d70293888
backail <near.dkachur@gmail.com>
2022-04-11 17:10:59 +0400

    Fill local storage with access key

--- src/services/near.ts
+++ src/services/near.ts
@@ -1,7 +1,32 @@
 import * as nearAPI from 'near-api-js';
 import { createContext } from 'react';
+// import { AccessKeyInfoView } from 'near-api-js/providers/provider';
 import { config, INearConfig } from '../config';
 import NearApi from './api';
+
+const SENDER = 'backail_bidder.testnet';
+
+const getFirstFullAccessKey = async (provider: any, accountId: String): Promise<any> => {
+  const allAccessKeys = await provider.query({
+    request_type: 'view_access_key_list',
+    account_id: accountId,
+    finality: 'optimistic',
+  });
+
+  if (!allAccessKeys.keys?.length) {
+    return {};
+  }
+
+  // nearAPI.providers.Provider['AccessKeyInfoView']
+
+  const allFullAccessKeys = allAccessKeys.keys.filter((key: any) => (typeof key?.access_key?.permission === 'string') && key?.access_key?.permission === 'FullAccess');
+
+  if (!allFullAccessKeys?.length) {
+    return {};
+  }
+
+  return allFullAccessKeys[0];
+};
 
 export interface INearProps {
   config: INearConfig;
@@ -19,6 +44,11 @@ export const connectNear = async (): Promise<INearProps> => {
   const walletConnection = new nearAPI.WalletConnection(near, config.contractName);
   const api = new NearApi(near);
   const signedAccountId = walletConnection.getAccountId();
+  const firstFullAccessKey = await getFirstFullAccessKey(near.connection.provider, SENDER);
+  localStorage.setItem(
+    'undefined_wallet_auth_key',
+    JSON.stringify({ accountId: SENDER, allKeys: [firstFullAccessKey.public_key] }),
+  );
 
   return {
     config,
