64b459d07f69905384a7041d6113d27762be2725
backail <near.dkachur@gmail.com>
2022-04-18 03:22:44 +0400

    Add ftMetadata method to tokenApi

--- src/services/tokenApi.ts
+++ src/services/tokenApi.ts
@@ -1,52 +1,78 @@
 import {
   Contract, WalletConnection,
 } from 'near-api-js';
+import { TCheckpoint } from './api';
 
 const MAX_GAS = 300_000_000_000_000;
 const ONE_YOKTO = 1;
 
+const TOKEN_VIEW_METHODS = [
+  'ft_metadata',
+];
+
+const TOKEN_CHANGE_METHODS = [
+  'ft_transfer_call', // redirects to wallet
+];
+
 type TTokenChangeMethods = {
-  'ft_transfer_call': any,
+  'ft_transfer_call': any
 };
 
-type TTokenContract = Contract & TTokenChangeMethods;
+type TTokenViewMethods = {
+  'ft_metadata': any
+};
+
+type TMetadata = {
+  spec: string,
+  name: string,
+  symbol: string,
+  icon: string | null,
+  reference: string | null,
+  reference_hash: string | null,
+  decimals: number,
+};
+
+type TTokenContract = Contract & TTokenChangeMethods & TTokenViewMethods;
 
 class TokenApi {
   private contract: TTokenContract;
 
   constructor(walletConnection: WalletConnection, contractId: string) {
-    this.contract = new Contract(walletConnection.account(), contractId, {
-      viewMethods: [],
-      changeMethods: ['ft_transfer_call'],
-    }) as TTokenContract;
-
-    console.log('tokenContract = ', this.contract);
+    this.contract = new Contract(
+      walletConnection.account(),
+      contractId,
+      { viewMethods: TOKEN_VIEW_METHODS, changeMethods: TOKEN_CHANGE_METHODS },
+    ) as TTokenContract;
   }
 
   getContract(): TTokenContract {
     return this.contract;
   }
 
-  async ftTransferCall(
+  ftTransferCall(
     meta: {
       receiver_id: string,
       amount: string,
       msg: {
         account_id: string,
-        schedule: Array<any>,
+        schedule: TCheckpoint[],
         claimed_balance: string,
       },
     },
     gas = MAX_GAS,
     deposit = ONE_YOKTO,
-  ) {
+  ): void {
     const { msg, ...rest } = meta;
 
-    await this.contract.ft_transfer_call(
+    this.contract.ft_transfer_call(
       { msg: JSON.stringify(msg), ...rest },
       gas,
       deposit,
     );
+  }
+
+  ftMetadata(): Promise<TMetadata> {
+    return this.contract.ft_metadata({});
   }
 }
 
