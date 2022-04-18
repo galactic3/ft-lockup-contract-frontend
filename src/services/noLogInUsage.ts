24f942dcc7d5e2f2b6c5be220707dd51dcaaea0e
backail <near.dkachur@gmail.com>
2022-04-18 16:59:10 +0400

    Remove unused access keys import

--- src/services/noLogInUsage.ts
+++ src/services/noLogInUsage.ts
@@ -1,11 +1,4 @@
-import * as nearAPI from 'near-api-js';
-import { config } from '../config';
-
-type TViewMethods = {
-  'get_deposit_whitelist': any,
-};
-
-export const getFirstFullAccessKey = async (provider: any, accountId: String): Promise<any> => {
+const getFirstFullAccessKey = async (provider: any, accountId: String): Promise<any> => {
   const allAccessKeys = await provider.query({
     request_type: 'view_access_key_list',
     account_id: accountId,
@@ -25,33 +18,4 @@ export const getFirstFullAccessKey = async (provider: any, accountId: String): P
   return allFullAccessKeys[0];
 };
 
-export const importWhitelistedAccountsFullAccessKeys = async () => {
-  const keyStore = new nearAPI.keyStores.BrowserLocalStorageKeyStore();
-  const near = await nearAPI.connect({ headers: {}, keyStore, ...config });
-  const walletConnection = new nearAPI.WalletConnection(near, config.contractName);
-  const contract = new nearAPI.Contract(walletConnection.account(), config.contractName, {
-    viewMethods: ['get_deposit_whitelist'],
-    changeMethods: [],
-  }) as (nearAPI.Contract & TViewMethods);
-  const ownerAccountIds = await contract.get_deposit_whitelist() || [];
-
-  const { provider } = near.connection;
-
-  if (!ownerAccountIds?.length) {
-    throw Error('Zero lockup owners');
-  }
-
-  await Promise.all(
-    ownerAccountIds.map(async (ownerAccountId: string) => {
-      const firstFullAccessKey = await getFirstFullAccessKey(provider, ownerAccountId);
-      console.log('ffak = ', firstFullAccessKey);
-
-      localStorage.setItem(
-        `${config.contractName}_wallet_auth_key`,
-        JSON.stringify({ accountId: ownerAccountId, allKeys: [firstFullAccessKey.public_key] }),
-      );
-    }),
-  );
-
-  console.log('ownerAccountIds = ', ownerAccountIds);
-};
+export default getFirstFullAccessKey;
