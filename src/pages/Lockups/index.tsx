6aeead3a8a18bab32d8d1333df5c055832a21329
Alex Aunter <lxaunter@gmail.com>
2022-04-23 23:45:59 +0400

    refactor(lockups): split LockupsTable and lockups page

--- src/pages/Lockups/index.tsx
+++ src/pages/Lockups/index.tsx
@@ -1,15 +1,10 @@
 import {
   Paper,
-  Table,
-  TableBody,
-  TableCell,
   TableContainer,
-  TableHead,
-  TableRow,
 } from '@mui/material';
 import { useContext } from 'react';
-import Row from '../../components/table/row';
 import CreateLockup from '../../components/CreateLockup';
+import LockupsTable from '../../components/LockupsTable';
 import { INearProps, NearContext } from '../../services/near';
 import { TMetadata } from '../../services/tokenApi';
 
@@ -32,24 +27,7 @@ export default function Lockups({ lockups, token, adminControls }: { lockups: an
     <div className="container">
 
       <TableContainer sx={{ boxShadow: 'unset', margin: '0 0 20px' }} component={Paper}>
-        <Table className="main-table" aria-label="collapsible table">
-          <TableHead className="table-head">
-            <TableRow>
-              <TableCell />
-              <TableCell align="left">ID</TableCell>
-              <TableCell align="left">Account ID</TableCell>
-              <TableCell align="right">Start&nbsp;date</TableCell>
-              <TableCell align="right">End&nbsp;date</TableCell>
-              <TableCell align="right">Total&nbsp;amount</TableCell>
-              <TableCell align="center">Progress</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {lockups.map((lockup) => (
-              <Row key={lockup.id} row={lockup} token={token} adminControls={adminControls} />
-            ))}
-          </TableBody>
-        </Table>
+        <LockupsTable lockups={lockups} token={token} adminControls={adminControls} />
       </TableContainer>
 
       {signedIn && adminControls && isAdmin && <CreateLockup token={token} />}
