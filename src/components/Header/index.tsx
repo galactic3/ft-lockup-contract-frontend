42c2d1f3f2066758cf4759daba59fe698031fd30
Alex Aunter <lxaunter@gmail.com>
2022-04-27 21:47:23 +0400

    feat(routing): add /admin/about page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -20,7 +20,7 @@ export default function Header({ adminControls }: { adminControls: boolean }) {
         <Box sx={{ display: 'flex' }}>
           <div className="nav">
             <Link className="nav-link" to={adminControls && isAdmin ? '/admin/lockups' : '/lockups'}>Lockups</Link>
-            <Link className="nav-link" to="/about">About</Link>
+            <Link className="nav-link" to={adminControls ? '/admin/about' : '/about'}>About</Link>
             {adminControls && (<Link className="nav-link" to="/admin/draft_groups">Draft Groups</Link>)}
           </div>
           {adminControls && signedIn && (
