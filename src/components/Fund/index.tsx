1aedfe5d53779730e2ea4992420cc7bc42082049
backail <near.dkachur@gmail.com>
2022-05-02 17:33:13 +0400

    Fund dao from admin wallet

---
+++ src/components/Fund/index.tsx
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+import { INearProps, NearContext } from '../../services/near';
+
+function Fund(props: { draftGroupIndex: number | undefined, amount: string | undefined }) {
+  const { near }: { near: INearProps | null } = useContext(NearContext);
+  const { draftGroupIndex, amount } = props;
+
+  if (!near) {
+    throw Error('Cannot access lockup api');
+  }
+
+  if (draftGroupIndex === undefined) {
+    throw Error('Cannot fund draft group without index');
+  }
+
+  if (amount === undefined) {
+    throw Error('Cannot fund draft group without specified amount');
+  }
+
+  const handleFund = () => {
+    near.tokenApi.fundDraftGroup(
+      near.api.getContract().contractId,
+      draftGroupIndex,
+      amount,
+    );
+  };
+
+  return (
+    <button className="button" type="button" onClick={handleFund}>Fund</button>
+  );
+}
+
+export default Fund;
