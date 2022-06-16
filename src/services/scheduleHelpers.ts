96800ef5b414985bd77884d9c170bd38db4399ff
donets <mmf.donec@gmail.com>
2022-06-17 01:07:29 +0300

    add update

--- src/services/scheduleHelpers.ts
+++ src/services/scheduleHelpers.ts
@@ -1,5 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const interpolate = (x0: number, y0: number, x1: number, y1: number, xM: number) => {
+
+import { TCheckpoint, TNearTimestamp } from './api';
+
+export const interpolateRaw = (x0: number, y0: number, x1: number, y1: number, xM: number) => {
   if (x1 <= x0) {
     throw new Error('invalid range');
   }
@@ -10,4 +13,16 @@ export const interpolate = (x0: number, y0: number, x1: number, y1: number, xM: 
   const yM = y0 + (y1 - y0) * (xM - x0) / (x1 - x0);
 
   return yM;
+};
+
+export const interpolate = (checkpoint0: TCheckpoint, checkpoint1: TCheckpoint, timestamp: TNearTimestamp) => {
+  const balance = interpolateRaw(
+    checkpoint0.timestamp,
+    parseFloat(checkpoint0.balance),
+    checkpoint1.timestamp,
+    parseFloat(checkpoint1.balance),
+    timestamp,
+  );
+
+  return { timestamp, balance: balance.toString() };
 };
