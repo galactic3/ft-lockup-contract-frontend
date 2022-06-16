012e56186f43bdc2e90b618e7d3a0d9c005fe558
donets <mmf.donec@gmail.com>
2022-06-17 00:40:02 +0300

    add test interpolate

--- src/services/scheduleHelpers.test.ts
+++ src/services/scheduleHelpers.test.ts
@@ -1,7 +1,7 @@
-import { foo } from './scheduleHelpers';
+import { interpolate } from './scheduleHelpers';
 
 describe('1 test', () => {
-  it('test sum', () => {
-    expect(foo).toStrictEqual(42);
+  it('test interpolate doesn\'t throw', () => {
+    interpolate(1_500_000_000, 10_000, 1_700_000_000, 14_000, 1_550_000_000);
   });
 });
