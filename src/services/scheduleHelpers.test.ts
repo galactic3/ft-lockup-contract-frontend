8fe76d112b308e8d9161c784ae14f9333c7eec8d
donets <mmf.donec@gmail.com>
2022-06-17 01:07:29 +0300

    add update

--- src/services/scheduleHelpers.test.ts
+++ src/services/scheduleHelpers.test.ts
@@ -1,25 +1,25 @@
-import { interpolate } from './scheduleHelpers';
+import { interpolate, interpolateRaw } from './scheduleHelpers';
 
 describe('1 test', () => {
   it('test interpolate doesn\'t throw', () => {
-    interpolate(1_500_000_000, 10_000, 1_700_000_000, 14_000, 1_550_000_000);
+    interpolateRaw(1_500_000_000, 10_000, 1_700_000_000, 14_000, 1_550_000_000);
   });
   it('raises error if invalid input', () => {
-    expect(() => interpolate(
+    expect(() => interpolateRaw(
       1_800_000_000,
       10_000,
       1_700_000_000,
       14_000,
       1_550_000_000,
     )).toThrow('invalid range');
-    expect(() => interpolate(
+    expect(() => interpolateRaw(
       1_500_000_000,
       10_000,
       1_700_000_000,
       14_000,
       1_450_000_000,
     )).toThrow('xM out of bound');
-    expect(() => interpolate(
+    expect(() => interpolateRaw(
       1_500_000_000,
       10_000,
       1_700_000_000,
@@ -28,9 +28,19 @@ describe('1 test', () => {
     )).toThrow('xM out of bound');
   });
   it('returns starting value at the beginning of range', () => {
-    expect(interpolate(1_500_000_000, 10_000, 1_700_000_000, 14_000, 1_500_000_000)).toStrictEqual(10_000);
+    expect(interpolateRaw(1_500_000_000, 10_000, 1_700_000_000, 14_000, 1_500_000_000)).toStrictEqual(10_000);
   });
   it('returns valid intermediate value', () => {
-    expect(interpolate(1_500_000_000, 10_000, 1_700_000_000, 14_000, 1_550_000_000)).toStrictEqual(11_000);
+    expect(interpolateRaw(1_500_000_000, 10_000, 1_700_000_000, 14_000, 1_550_000_000)).toStrictEqual(11_000);
+  });
+});
+
+describe('1 test', () => {
+  it('returns valid intermediate value', () => {
+    expect(interpolate(
+      { timestamp: 1_500_000_000, balance: '10000' },
+      { timestamp: 1_700_000_000, balance: '14000' },
+      1_550_000_000,
+    )).toStrictEqual({ timestamp: 1_550_000_000, balance: '11000' });
   });
 });
