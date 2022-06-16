9422b10e5c581d5cfc04192d2ee7f013af7aac2a
donets <mmf.donec@gmail.com>
2022-06-17 00:40:02 +0300

    add test interpolate

--- src/services/scheduleHelpers.ts
+++ src/services/scheduleHelpers.ts
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
-export const foo = 42;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const interpolate = (x0: number, y0: number, x1: number, y1: number, xM: number) => 0;
