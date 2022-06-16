1db2da40244e5ebc9b8a40b738b14ba4c436ad51
donets <mmf.donec@gmail.com>
2022-06-17 00:47:46 +0300

    add some test

--- src/services/scheduleHelpers.test.ts
+++ src/services/scheduleHelpers.test.ts
@@ -4,4 +4,27 @@ describe('1 test', () => {
   it('test interpolate doesn\'t throw', () => {
     interpolate(1_500_000_000, 10_000, 1_700_000_000, 14_000, 1_550_000_000);
   });
+  it('raises error if invalid input', () => {
+    expect(() => interpolate(
+      1_800_000_000,
+      10_000,
+      1_700_000_000,
+      14_000,
+      1_550_000_000,
+    )).toThrow('invalid range');
+    expect(() => interpolate(
+      1_500_000_000,
+      10_000,
+      1_700_000_000,
+      14_000,
+      1_450_000_000,
+    )).toThrow('xM out of bound');
+    expect(() => interpolate(
+      1_500_000_000,
+      10_000,
+      1_700_000_000,
+      14_000,
+      1_750_000_000,
+    )).toThrow('xM out of bound');
+  });
 });
