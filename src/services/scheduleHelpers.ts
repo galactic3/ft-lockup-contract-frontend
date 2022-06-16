512dcf3a4949cffcb9c6c28ac3f33a62e90e4b99
donets <mmf.donec@gmail.com>
2022-06-17 00:47:46 +0300

    add some test

--- src/services/scheduleHelpers.ts
+++ src/services/scheduleHelpers.ts
@@ -1,2 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const interpolate = (x0: number, y0: number, x1: number, y1: number, xM: number) => 0;
+export const interpolate = (x0: number, y0: number, x1: number, y1: number, xM: number) => {
+  if (x1 <= x0) {
+    throw new Error('invalid range');
+  }
+  if (xM < x0 || xM >= x1) {
+    throw new Error('xM out of bound');
+  }
+
+  return x0;
+};
