412825793ff681bafa774dbbdbd9b6454f0eadd8
donets <mmf.donec@gmail.com>
2022-06-21 05:46:53 +0300

    fix

--- src/NearApp.test.tsx
+++ src/NearApp.test.tsx
@@ -1,42 +1,34 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import NearApp from './NearApp';
 
 jest.mock('./services/near', () => ({
   ...jest.requireActual('./services/near'),
-  connectNear: async () => {
-    return {
-      config: {},
-      api: {
-        loadAllLockups: async () => {
-          return Promise.resolve([]);
-        }
-      },
-      tokenApi: {
-        getContract: () => { return { contractId: 'token.jest.testnet'} },
-        ftMetadata: () => { return {
-          spec: 'ft-1.0.0',
-          name: 'Token',
-          symbol: 'TOKEN',
-          icon: null,
-          reference: null,
-          reference_hash: null,
-          decimals: 6
-        } },
-      },
-      signedIn: false,
-      signedAccountId: '',
-    }
-  }
+  connectNear: async () => ({
+    config: {},
+    api: {
+      loadAllLockups: async () => Promise.resolve([]),
+    },
+    tokenApi: {
+      getContract: () => ({ contractId: 'token.jest.testnet' }),
+      ftMetadata: () => ({
+        spec: 'ft-1.0.0',
+        name: 'Token',
+        symbol: 'TOKEN',
+        icon: null,
+        reference: null,
+        reference_hash: null,
+        decimals: 6,
+      }),
+    },
+    signedIn: false,
+    signedAccountId: '',
+  }),
 }));
-
 
 it('renders welcome message', async () => {
   await act(async () => {
     render(<NearApp />);
     await new Promise(process.nextTick);
   });
-  const progressItem= await screen.findAllByText(/Progress/);
-  expect(progressItem).toHaveLength(1);
 });
