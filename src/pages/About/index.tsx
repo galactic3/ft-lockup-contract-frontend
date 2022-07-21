d2e0b91c3185bac86c4994e28301882462c2a388
donets <mmf.donec@gmail.com>
2022-07-21 06:08:04 +0300

    add paragraphs to about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -10,23 +10,25 @@ export default function About(
       <h2>Purpose</h2>
 
       <div>
-        Community Lockup is dApp implementing locking and vesting of any arbitrary fungible token on NEAR in the generally known sense of these words.
-        <br />
-        Lockup is a gradual release of tokens according to a specified schedule. After release tokens are in full control of the beneficiary.
-        <br />
-        Vesting is a gradual allocation of tokens to the beneficiary according to a specified schedule, provided certain conditions are met (for example, beneficiary keeps working for the payer).
-        <br />
-        Vested tokens are NOT immediately available to the beneficiary, however
-        they WILL be available to the beneficiary according to the lockup
-        schedule. Vested tokens are GUARANTEED to be received at some point in
-        the future, while unvested token amount may be withdrawn by the payer if
-        the lockup agreement is terminated.
+        <p>Community Lockup is dApp implementing locking and vesting of any arbitrary fungible token on NEAR in the generally known sense of these words.</p>
+        <p>Lockup is a gradual release of tokens according to a specified schedule. After release tokens are in full control of the beneficiary.</p>
+        <p>
+          Vesting is a gradual allocation of tokens to the beneficiary according to a specified schedule,
+          provided certain conditions are met (for example, beneficiary keeps working for the payer).
+        </p>
+        <p>
+          Vested tokens are NOT immediately available to the beneficiary, however
+          they WILL be available to the beneficiary according to the lockup
+          schedule. Vested tokens are GUARANTEED to be received at some point in
+          the future, while unvested token amount may be withdrawn by the payer if
+          the lockup agreement is terminated.
+        </p>
       </div>
 
       <h2>Users</h2>
 
       <div>
-        There are two roles in the app: payer (also called admin), most commonly the employer, and beneficiaries, most commonly the employees. There is also an implicit “anyone” role.
+        <p>There are two roles in the app: payer (also called admin), most commonly the employer, and beneficiaries, most commonly the employees. There is also an implicit “anyone” role.</p>
 
         <ul>
           <li>
@@ -44,7 +46,7 @@ export default function About(
       <h2>Lockups</h2>
 
       <div>
-        Each lockup has the following fields:
+        <p>Each lockup has the following fields:</p>
         <ul>
           <li>Account ID: NEAR account id that will receive the tokens</li>
           <li>
@@ -76,24 +78,15 @@ export default function About(
       <h2>Lockup and vesting logic</h2>
 
       <div>
-        <div>
-          <img src="https://bafkreidqdq6wefcvr436cunap4y5c77nodlgxu6c4qchxkd232g7377cw4.ipfs.nftstorage.link/" alt="Lockup and vesting schedule" />
-        </div>
-        Consider the following lockup with a four year duration.
-        <br />
-        The vesting schedule is red. One year cliff, then 1 / 4 is vested, then vesting is linear till the end (4 years mark).
-        <br />
-        The lockup schedule is blue. Two years cliff with zero amount. Then two years of linear lockup till 3 / 4 of the amount, then another cliff up to full amount.
-        <br />
-        Consider the termination happening at the 2.5 years mark.
-        <br />
-        First, we find the vested amount for this timestamp. It is 5 / 8.
-        <br />
-        After that we trim the lockup schedule at that amount.
-        <br />
-        In this case, the intersection is at 3y + 1 / 3y.
-        <br />
-        The trimmed lockup schedule is the yellow thin line. Note that it perfectly matches the initial lockup schedule up to the new total ( 5 / 8 ).
+        <img src="https://bafkreidqdq6wefcvr436cunap4y5c77nodlgxu6c4qchxkd232g7377cw4.ipfs.nftstorage.link/" alt="Lockup and vesting schedule" />
+        <p>Consider the following lockup with a four year duration.</p>
+        <p>The vesting schedule is red. One year cliff, then 1 / 4 is vested, then vesting is linear till the end (4 years mark).</p>
+        <p>The lockup schedule is blue. Two years cliff with zero amount. Then two years of linear lockup till 3 / 4 of the amount, then another cliff up to full amount.</p>
+        <p>Consider the termination happening at the 2.5 years mark.</p>
+        <p>First, we find the vested amount for this timestamp. It is 5 / 8.</p>
+        <p>After that we trim the lockup schedule at that amount.</p>
+        <p>In this case, the intersection is at 3y + 1 / 3y.</p>
+        <p>The trimmed lockup schedule is the yellow thin line. Note that it perfectly matches the initial lockup schedule up to the new total ( 5 / 8 ).</p>
       </div>
 
       <h2>Technical Info</h2>
