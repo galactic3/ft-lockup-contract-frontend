203756e8e5a0d9080e7d1bc732e34fdce0f955ea
Alex Aunter <lxaunter@gmail.com>
2022-07-28 02:44:40 +0400

    feat: factory validate token id

--- src/pages/NewLockupContract/index.tsx
+++ src/pages/NewLockupContract/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Box } from '@mui/material';
 import { useSnackbar } from 'notistack';
@@ -12,6 +12,27 @@ export default function NewLockupContract() {
 
   const { enqueueSnackbar } = useSnackbar();
   const { near, signIn }: { near: INearProps | null, signIn: () => void } = useContext(NearContext);
+
+  const [tokenAccountId, setTokenAccountId] = useState<string>('');
+
+  const validateAccountExists = async (accountId: string) => {
+    if (!near) throw new Error('unreachable');
+    if (accountId.match(/^[0-9a-f]{64}$/)) {
+      return;
+    }
+    try {
+      const { total } = (await (await near.near.account(accountId)).getAccountBalance());
+      console.log(`${accountId} balance: ${total}`);
+    } catch (e) {
+      const message = `Account ID "${accountId}" does not exist`;
+      throw new Error(message);
+    }
+  };
+
+  const handleChangeTokenAccountId = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setTokenAccountId(value);
+  };
 
   const handleDeployLockupContract = (e: any) => {
     e.preventDefault();
@@ -27,7 +48,15 @@ export default function NewLockupContract() {
           .split(',').map((x: string) => x.trim()).filter((x: string) => x.length > 0);
         const draftOperators: string[] = e.target.elements.draft_operators.value
           .split(',').map((x: string) => x.trim()).filter((x: string) => x.length > 0);
-        const tokenAccountId: string = e.target.elements.token_account_id.value;
+
+        const allUniqueAccountIds = Array.from(new Set(
+          [...lockupOperators, ...draftOperators, tokenAccountId],
+        ));
+        for (let i = 0; i < allUniqueAccountIds.length; i += 1) {
+          const accountId = allUniqueAccountIds[i];
+          await validateAccountExists(accountId);
+        }
+
         const lockupSubaccountId: string = e.target.elements.lockup_subaccount_id.value;
 
         if (lockupOperators.length === 0) {
@@ -116,14 +145,19 @@ export default function NewLockupContract() {
             </div>
             <div className="form-row">
               <span>Fungible token contract address: </span>
-              <input type="text" id="token_account_id" />
+              <input
+                type="text"
+                id="token_account_id"
+                value={tokenAccountId}
+                onChange={handleChangeTokenAccountId}
+              />
             </div>
             <div className="form-row">
               <span>New Lockup Contract Address: </span>
               <input type="text" id="lockup_subaccount_id" />
             </div>
 
-            <button className="button submit" type="submit" disabled={!near.currentUser.signedAccountId}>
+            <button className="button submit" type="submit" disabled={!(near.currentUser.signedAccountId && tokenAccountId)}>
               Deploy Contract
             </button>
           </div>
